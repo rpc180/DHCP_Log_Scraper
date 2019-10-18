d965f3e19be143fe73749b51b805d3aa5e578d45
Romel <43550213+rpc180@users.noreply.github.com>
2019-10-18 12:25:07 -0400

    Create Latest_DHCP_Assignments.ps1
    Initial Commit

---
+++ Latest_DHCP_Assignments.ps1
@@ -0,0 +1,47 @@
+$newest = @()
+$line = "" | select tick,device,ip,stamp
+$line.tick = ""
+$line.device = ""
+$line.ip = ""
+$line.stamp = ""
+$newest+=$line
+
+$devices = get-content dhcpsrvlog* | select-string -pattern ',Renew,',',Request,'
+
+write-output "
+
+...Scraping dhcpv4 logs for Renew and Request patterns"
+
+foreach ( $reader in $devices ) {
+    $split = $reader -split ","
+    $timestr = $split[1]+" "+$split[2]
+    $timestamp = get-date $timestr
+    $tickeq = ($timestamp).ticks
+    $ip = $split[4]
+    $device = $split[6]
+    $newobj = [PSCustomObject]@{ 'Tick' = $tickeq; 'Device' = $device; 'IP' = $ip; 'Stamp' = $timestamp }
+    if ($newest.Device.contains($device)) {
+        $indexnum = $newest.device.indexof($device)
+        write-verbose "Found $device at position $indexnum"
+        write-verbose "$timestamp EVALUATE"
+        write-verbose "$($newest.stamp[$indexnum]) FOUND"
+        if ($newest.tick[$indexnum] -lt $tickeq) { 
+            write-verbose "$timestamp NEWER THAN $($newest[$indexnum].stamp)"
+            $newest[$indexnum].tick = $tickeq
+            $newest[$indexnum].ip = $ip
+            $newest[$indexnum].stamp = $timestamp 
+            write-verbose "Timestamp now $($newest[$indexnum].stamp)"}
+        else {
+            write-verbose "$timestamp OLDER THAN $($newest[$indexnum].stamp), no update needed"
+            } 
+        }
+    else {
+        write-output "No previous record, adding $device to array"
+        $newest+=$newobj 
+        }
+    }
+
+$newest = $newest[1..($newest.length-1)] #Remove null first initialization row
+write-output "
+...Current DHCP assignments, total elements $($newest.count):"
+$newest | sort IP -descending | ft IP,Device,Stamp
